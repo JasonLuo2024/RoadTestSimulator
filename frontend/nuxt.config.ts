6d0b84e27b12d67ecef087001cbe03e9e74b4ad8
Haoming Luo <hluo@mun.ca>
2024-10-02 14:35:56 -0230

    Set up deployment to GitHub Pages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  target: "static", // Set the target to 'static'
+  router: {
+    base: "/RoadTestSimulator/", // Make sure to include leading and trailing slashes
+  },
 });
