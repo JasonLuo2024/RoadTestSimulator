ec4c47d6d4339712f112ea6c19946d7a02768121
Haoming Luo <hluo@mun.ca>
2024-10-04 14:47:58 -0230

    project update

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,6 +2,11 @@
   <div
     class="h-screen w-screen flex flex-col justify-center items-center overflow-hidden bg-[#e6e5de]"
   >
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+    />
+
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
@@ -327,5 +332,9 @@ onMounted(() => {
 
   background-position: center;
   background-size: 400px 560px; /* Set the background size */
+}
+
+button {
+  touch-action: manipulation;
 }
 </style>
